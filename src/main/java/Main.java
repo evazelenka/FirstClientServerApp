02b90501ac977d6f2c7300d717121fdc7c766997
zelenka <eva.zelya@gmail.com>
2025-06-17 14:56:30 +0300

    sign up added

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Client.ClientGUI;
+import Server.ServerWindow;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow server = new ServerWindow();
+        new ClientGUI(server);
+        new ClientGUI(server);
+    }
+}
